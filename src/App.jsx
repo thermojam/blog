ec4c04faacc6777641bbdf6c882250335deffa47
nikita <nmensky@gmail.com>
2025-07-14 18:27:32 +0300

    initial project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+const Div = styled.div`
+text-align: center;
+color: blue;`
+
+export const App = () => {
+
+    return (
+        <Div>
+            Hello World!
+        </Div>
+
+    )
+}
+
+
