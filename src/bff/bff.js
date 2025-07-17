e46190bcf2f4ee1d1baa677d23757b6f255be8cb
nikita <nmensky@gmail.com>
2025-07-17 21:18:19 +0300

    Initial implementation of BFF (registration)

--- src/bff/bff.js
+++ src/bff/bff.js
@@ -1,3 +1,7 @@
+const generateDate = () => new Date(Math.random() * 1000000000000 + 1999999999).
+    toISOString().substr(0, 16).
+    replace('T', ' ');
+
 export const server = {
     async authorize(authLogin, authPassword) {
         const users = await fetch('http://localhost:3001/users')
@@ -36,5 +40,51 @@ export const server = {
             error: null,
             res: session,
         }
+    },
+
+    async register(regLogin, regPassword) {
+        const users = await fetch('http://localhost:3001/users')
+            .then((loadedUsers) => loadedUsers.json(),)
+
+        const user = users.find(({login}) => login === regLogin)
+
+        if (user) {
+            return {
+                error: "Такой логин уже занят",
+                res: null,
+            }
+        }
+
+        await fetch('http://localhost:3001/users', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json; charset=utf-8',
+            },
+            body: JSON.stringify({
+                login: regLogin,
+                password: regPassword,
+                registed_at: generateDate(),
+                role_id: 2,
+            }),
+        })
+
+        const session = {
+
+            logOut() {
+                Object.keys(session).forEach((key) => {
+                    delete session[key];
+                })
+            },
+
+            removeComment() {
+                console.log('Удаление комментария')
+            },
+        }
+
+        return {
+            error: null,
+            res: session,
+        }
+
     }
 }
