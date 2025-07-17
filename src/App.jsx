d165ef072689f9816d4aef5a067e674f8f202823
nikita <nmensky@gmail.com>
2025-07-17 18:24:38 +0300

    preparing a project for development

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import styled from 'styled-components'
 
 const Div = styled.div`
-text-align: center;
-color: blue;`
+    text-align: center;
+    color: #5991ff;
+    margin: 20px;
+    font-size: 40px;`
 
 export const App = () => {
 
     return (
         <Div>
             Hello World!
+            <i className="fa fa-address-card-o"></i>
         </Div>
 
     )
